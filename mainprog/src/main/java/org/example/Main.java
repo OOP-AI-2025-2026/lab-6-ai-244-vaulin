f5facd85ff5cb9e348e2b376baf1aa2b3a73a10a
Osssy010101 <ossys1010110@gmail.com>
2025-10-23 16:28:10 +0300

    did task1

--- mainprog/src/main/java/org/example/Main.java
+++ mainprog/src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Dog myDog = new Dog("Dog1");
+
+        Cat myCat = new Cat("Cat1");
+
+        System.out.println(myDog.getName() + " says: " + myDog.makeSound());
+        System.out.println(myCat.getName() + " says: " + myCat.makeSound());
     }
 }
